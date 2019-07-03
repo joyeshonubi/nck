dcef64f5e9f077bcafb9decf92ab5618237eecf2
joye <shonubij@gmail.com>
2019-07-03 20:18:36 +0100

    promo code update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+import './App.css';
+import { Row,  Button } from 'react-bootstrap';
+import Card from "./Card"
+import  FormComponent from './Form'
+import Summary from './Summary'
+
+class App extends Component {
+  state = {
+    
+    card: '',
+    date: ''
+  }
+
+  handleChange =(e) =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    console.log(this.state)
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="container">
+    <nav>
+    <ul>
+    <li>Trips</li>
+    <li>Recently viewed</li>
+    <li>Bookings</li>
+    <img src ="images/avatar2.png" alt= "Profile" />
+    </ul>
+    </nav>
+    <hr/>
+
+
+    <div className ="container section-2" >
+    
+    <div className= "details-head ">
+    <div>
+    <h2 id="payment-info">Payment Information</h2>
+    <p id ="choose-method">Choose your method of payment</p>
+    </div>
+    
+
+    <div className ="logos">
+    <img src = 'images/visa.svg' alt="Visa" />
+    <img src = 'images/discover.svg' alt="Discover" />
+    <img src = 'images/paypal.svg' alt="Paypal" />
+   
+    </div>
+    </div>
+    
+    <Row >
+    <Card state ={this.state} />
+    <FormComponent handleChange ={this.handleChange} state= {this.state}/>
+    </Row>
+   
+    <hr/>
+    </div>
+    <Summary/>
+    <hr/>
+    <section className ="final container">
+    <Button className="paynow" size="lg">Complete payment</Button>
+    <p className="total"> TOTAL :&#8358;2556.00</p>
+    </section>
     </div>
   );
+  }
 }
 
 export default App;
